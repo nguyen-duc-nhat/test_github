c1f11b366f8159ed063a5d6f286c9af75b59dfe8
NhatDucNguyen <ducnhat220699@gmail.com>
2022-08-01 22:05:40 +0700

    hello world commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function helloWorld(){
+    console.log('Hello world!');
+}
+
+function helloWorld2(){
+    console.log('Hello world 2');
+}
+
+function helloWorld3(){
+    console.log('Hello world 3');
+}
