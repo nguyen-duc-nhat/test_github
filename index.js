13d6617da7bbefec0319740134263179e73e6d4a
NhatDucNguyen <ducnhat220699@gmail.com>
2022-08-01 22:28:01 +0700

    Comment hello world!

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function helloWorld2(){
     console.log('Hello world 2');
 }
 
-function helloWorld3(){
-    console.log('Hello world 3');
-}
+// function helloWorld3(){
+//     console.log('Hello world 3');
+// }
